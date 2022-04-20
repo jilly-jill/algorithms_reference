a5f72004a34a9bbca439907ed35dac6d1b644966
Jillian Sechrest <jillian.sechrest@bewaterwsnc.com>
2022-04-20 17:01:33 -0400

    com.algorithms.BubbleSort.java-added-js

---
+++ src/com/algorithms/partition/Partition.java
@@ -0,0 +1,39 @@
+package com.algorithms.partition;
+
+public class Partition {
+
+    /*
+
+Import QuickSort.java into an IntelliJ project
+Implement partition and swap methods
+
+Part A
+a.   private int partition(int[] numbers, int start, int end)
+b.   private void swap(int[] numbers, int j, int k)
+Implement the private sort method
+a.   private void sort(int[] numbers, int start, int end) Run main to see the overall functionality
+Make a test plan, create a test class and implement test cases
+
+Merge Sort Pseudo-code
+
+mergeSort(array, start, end)
+if start<end
+middle = (end-start)/2 + start mergeSort(array, start, middle)
+mergeSort(array, middle+1, end)
+merge(array, start, middle, end)
+
+     */
+
+    public static void main(String[] args) {
+
+        int[] numbers = {3, 7, 9, 6, 11, 8};
+    }
+
+    private static void partition(int[] numbers){
+
+
+    }
+    }
+
+
+
