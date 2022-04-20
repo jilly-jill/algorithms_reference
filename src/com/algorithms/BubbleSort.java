282a9a035b65091d8474e525dc8b9e01b0b7a853
Jillian Sechrest <jillian.sechrest@bewaterwsnc.com>
2022-04-19 23:04:15 -0400

    com.algorithms.BubbleSort.java-added-js

--- src/com/algorithms/BubbleSort.java
+++ src/com/algorithms/BubbleSort.java
@@ -1,4 +1,36 @@
 package com.algorithms;
-
+import java.util.Arrays;
 public class BubbleSort {
+
+
+    public static void main(String[] args) {
+
+        int[] numbers = {4, 15, 2, 6, 9, 22, 13};
+        Arrays.toString(numbers);
+        improve(numbers);
+        String improved = Arrays.toString(numbers);
+        System.out.println(improved);
+
+
+    }
+
+    public static void improve(int[] numbers) {
+        if (numbers != null) {
+            for (int i = 1; i < numbers.length; i++) {
+                for (int j = 0; j < numbers.length - 1; j++) {
+                    if (numbers[j] > numbers[j + 1]) {
+                        swap(numbers, j, j + 1);
+                    }
+                }
+            }
+        }
+    }
+
+
+    private static void swap(int[] numbers, int j, int k) {
+        int temp = numbers[j];
+        numbers[j] = numbers[k];
+        numbers[k] = temp;
+
+    }
 }
